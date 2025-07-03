a7474079e7327ced9b395a80a0c63048a8a83d1f
Adeniji Olajide <adenijiolajid01@gmail.com>
2025-07-03 12:25:55 +0100

    fix: correct export of certOptions after existence check for certificate files

--- utils/cert.js
+++ utils/cert.js
@@ -4,9 +4,11 @@ import process from "process";
 const certdir = "C:\\Users\\adeni\\Documents\\Cert\\";
 
 if (fs.existsSync(`${certdir}localhost.key`) && fs.existsSync(`${certdir}localhost.crt`)) {
-  export const certOptions = {
+  const certOptions = {
     key: fs.readFileSync(`${certdir}localhost.key`),
     cert: fs.readFileSync(`${certdir}localhost.crt`),
     passphrase: process.env.CERT_PASSWORD
   };
 }
+
+export { certOptions };
