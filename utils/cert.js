dedfd211750494032c75f0f01ed5ce688e332b97
Adeniji Olajide <adenijiolajid01@gmail.com>
2025-07-03 12:26:24 +0100

    fix: correct variable scope for certOptions in certificate export

--- utils/cert.js
+++ utils/cert.js
@@ -2,9 +2,9 @@ import fs from "fs";
 import process from "process";
 
 const certdir = "C:\\Users\\adeni\\Documents\\Cert\\";
-
+let certOptions = {};
 if (fs.existsSync(`${certdir}localhost.key`) && fs.existsSync(`${certdir}localhost.crt`)) {
-  const certOptions = {
+  certOptions = {
     key: fs.readFileSync(`${certdir}localhost.key`),
     cert: fs.readFileSync(`${certdir}localhost.crt`),
     passphrase: process.env.CERT_PASSWORD
